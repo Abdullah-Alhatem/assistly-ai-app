332724876f3b7568b481f3b5cf66df28f3c84f8e
Abdullah Al-hatem <abdhatm42@gmail.com>
2025-11-14 13:37:44 +0300

    make the chatbot for public

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
   '/chatbot/(.*)',
+  '/api/send-message',
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
